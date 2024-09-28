b1b8fd8e70c9910a016dd037daba889f129b8bef
Trung Nam <trungnam.work@gmail.com>
2024-09-28 19:19:34 +0700

    feat(ui,api): Search Exercises and fetch data

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+        'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = response.json();
+
+    return data;
+}
